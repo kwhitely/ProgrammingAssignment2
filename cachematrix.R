60cd38a50a14ef4c9134d3dab3c657d5c9695ee8
kwhitely <kwhitely@hotmail.com>
2014-11-18 14:39:01 -0600

    Updated file containing Assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate the inverse of a square matrix, storing the 
+##result in cache for faster access
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a matrix object that can cache its inverse
+makeCacheMatrix<- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL                              # Sets initial value to null for cacheSolve
+  }                                               #function to determine if inverse is in cache
+  get <- function() x 
+  setinverse <- function(solve) m <<- solve       # Inverse is calculated and stored in cache
+  getinverse <- function() m
+  list(set = set, get = get,                      # Sets names so values can be called
+       setinverse = setinverse,                   
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the matrix returned by makeCacheMatrix.  If already computed,
+##the function will retrieve the inverse from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {                               # Displays message if inverse 
+    message("getting cached data")          #is already stored in cache
+    return(m)                               #and then displays inverse
+  }
+  data <- x$get()                                 # Gets matrix from get()
+  m <- solve(data, ...)                           # Calculates inverse if not in cache
+  x$setinverse(m)                                 # Calls setinverse function to store
+  m                                               #inverse in cache
 }
